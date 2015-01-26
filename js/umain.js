081e5346a3136088cf1cb8d74fb86f34e5ea8120
lucasflores <lflor017@ucr.edu>
2015-01-26 15:19:43 -0800

    attempt at mobile friendly main page

--- js/umain.js
+++ js/umain.js
@@ -54,11 +54,40 @@
 		
 		// Get window size
 	    winH = $window.height();
+	    winW = $window.width();
 	    
 	    // Keep minimum height 550
-	    if(winH <= 550) {
-			winH = 550;
+	    if(winH <= 650) {
+			winH = 650;
 		} 
+
+		    // Init Skrollr for 768 and up
+	    if( winW >= 768) {
+	 
+	        // Init Skrollr
+	        var s = skrollr.init({
+	            forceHeight: false
+	        });
+	 
+	        // Resize our slides
+	        $slide.height(winH);
+	 
+	        s.refresh($('.homeSlide'));
+	 
+	    } else {
+	 
+	        // Init Skrollr
+	        var s = skrollr.init();
+	        s.destroy();
+	    }
+	 
+	    // Check for touch
+	    if(Modernizr.touch) {
+	 
+	        // Init Skrollr
+	        var s = skrollr.init();
+	        s.destroy();
+	    }
 	    
 	    // Resize our slides
 	    $slide.height(winH);
@@ -69,6 +98,20 @@
 	    s.refresh($('.homeSlide'));
 	    
 	}
+
+	function initAdjustWindow() {
+    return {
+        match : function() {
+            adjustWindow();
+        },
+        unmatch : function() {
+            adjustWindow();
+        }
+    };
+	}
+
+
+	enquire.register("screen and (min-width : 768px)", initAdjustWindow(), false);
 
 	function initHomepageNav(){
 		
