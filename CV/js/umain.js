b2ee74ab7a57d4bbac2c0c29daaf05d88541f4ee
lucasflores <lflor017@ucr.edu>
2015-01-27 21:53:43 -0800

    cv mobile upd

--- CV/js/umain.js
+++ CV/js/umain.js
@@ -42,33 +42,72 @@
 	
 	function adjustWindow(){
 		
-		// Init Skrollr
-		var s = skrollr.init({
+				
+		// Get window size
+	    winH = $window.height();
+	    winW = $window.width();
+	    
+	    // Keep minimum height 550
+	    if(winH <= 650) {
+			winH = 650;
+		} 
+
+		    // Init Skrollr for 768 and up
+	    if( winW >= 768) {
+	 
+	        // Init Skrollr
+	        ///var s = skrollr.init({
+	        ///   forceHeight: false
+	        ///});
+
+	        // Init Skrollr
+			var s = skrollr.init({
 		    forceHeight: false,
 		    render: function(data) {
 		    
 		        //Debugging - Log the current scroll position.
 		        //console.log(data.curTop);
 		    }
-		});
-		
-		// Get window size
-	    winH = $window.height();
-	    
-	    // Keep minimum height 550
-	    if(winH <= 550) {
-			winH = 550;
-		} 
-	    
-	    // Resize our slides
-	    $slide.height(winH);
-	    $slideTall.height(winH*2);
-	    $slideTall2.height(winH*3);
-	    
-	    // Refresh Skrollr after resizing our sections
-	    s.refresh($('.homeSlide'));
+			});
+	 
+	        // Resize our slides
+	        $slide.height(winH);
+	        $slideTall.height(winH*2);
+	        $slideTall2.height(winH*3);
+	 
+	        // Refresh Skrollr after resizing our sections
+	    	s.refresh($('.homeSlide'));
+	 
+	    }
+	     else {
+	 
+	        // Init Skrollr
+	        var s = skrollr.init();
+	        s.destroy();
+	    }
+	 
+	    // Check for touch
+	    if(Modernizr.touch) {
+	 
+	        // Init Skrollr
+	        var s = skrollr.init();
+	        s.destroy();
+	    }	
 	    
 	}
+	
+	function initAdjustWindow() {
+    return {
+        match : function() {
+            adjustWindow();
+        },
+        unmatch : function() {
+            adjustWindow();
+        }
+    };
+	}
+
+	enquire.register("screen and (min-width : 768px)", initAdjustWindow(), false);
 
 	function initHomepageNav(){
 		
